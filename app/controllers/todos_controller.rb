d83edf9f4e1ab0944548294fa4c66f9b99554a35
patilsameer <sbpatil29@gmail.com>
2021-09-28 21:41:32 +0530

    working routes for todo and todo:id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,13 @@
 class TodosController < ApplicationController
     def index
         #render plain: "Hello, this is /todos!"
-        render plain: Todo.all.to_a
+        render plain: Todo.order(:due_date).map{ |todo| todo.to_pleasant_string }.join("\n")
+    end
+
+    def show
+        id=params[:id]
+        #render plain: "The id you wanted to render was #{id}"
+        todo=Todo.find(id)
+        render plain: todo.to_pleasant_string
     end
 end
