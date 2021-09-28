5e48fe12ff4006052c809d3f96e27abfd9d49af6
patilsameer <sbpatil29@gmail.com>
2021-09-29 00:04:05 +0530

    completed the users task for level 5

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def index
+        #render plain: "Hello, this is /todos!"
+        render plain: User.order(:name).map{ |user| user.to_pleasant_string }.join("\n")
+    end
+   
+    def show
+        id=params[:id]
+        #render plain: "The id you wanted to render was #{id}"
+        user=User.find(id)
+        render plain: user.to_pleasant_string
+    end
+
+    def create
+        name=params[:name]
+        email=params[:email]
+        password=params[:password]
+        new_user=User.create!(
+            name: name,
+            email: email,
+            password: password,
+        )
+        response_text = "created new user with id=#{new_user.id}"
+        render plain: response_text
+    end
+
+    def login
+        email=params[:email]
+        password=params[:password]
+        #if User.find_by_name(email).
+        if User.exists?(email: email,password: password)
+             render plain: "true"
+        else
+            render plain: "false"
+        end
+        #if user.password=password
+        #    render plain: "true"
+        #else
+        #    render plain: "false"        
+        #end
+    end
+    #def update
+    #    id=params[:id]
+    #    completed=params[:completed]
+    #    todo=Todo.find(id)
+    #    todo.completed=completed
+    #    todo.save!
+
+    #    render plain: "Updated todo completed status of #{id} to #{completed}"
+    #end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   #get "todos", to: "todos#index"
   #post "todos/create", to: "todos#create"
   #get "todos/:id", to: "todos#show"
+  post "users/create", to: "users#create"
   resources :todos
+  resources :users
+  post "users/login", to: "users#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
