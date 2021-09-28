bdc696bd0ff9ff71102b57934addbfb1ffc65c0b
patilsameer <sbpatil29@gmail.com>
2021-09-28 22:29:16 +0530

    using post method for creating record

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def index
         #render plain: "Hello, this is /todos!"
         render plain: Todo.order(:due_date).map{ |todo| todo.to_pleasant_string }.join("\n")
