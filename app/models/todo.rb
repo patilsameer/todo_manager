65890d038d70ed533cee2d6afd248e80e42b9459
patilsameer <sbpatil29@gmail.com>
2021-10-23 20:30:31 +0530

    database & ui rendering

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,12 @@ class Todo < ActiveRecord::Base
         is_completed = completed ? "[X]" : "[ ]"
         "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
     end
+
+    def self.due_tomorrow
+        Todo.all.where("due_date = ?",Date.tomorrow)
+    end
+
+    def self.overdue
+        Todo.all.where("due_date < ?",Date.today)
+    end 
 end
